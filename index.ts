e7268e2a1c2a88ae1211617770c744979d1b0926
y-solb <ysolb99@gmail.com>
2024-01-19 00:00:09 +0900

    Feat: OpenAI 초기셋팅

---
+++ index.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function main() {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "Say this is a test" }],
+    model: "gpt-3.5-turbo",
+  });
+}
+
+main();
