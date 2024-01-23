da58393216555bbbb3a704213257eea88c2afd96
y-solb <ysolb99@gmail.com>
2024-01-23 13:14:27 +0900

    Feat: 프론트 구현

---
+++ frontend/index.js
@@ -0,0 +1,36 @@
+document.getElementById("submitBtn").addEventListener("click", () => {
+  const APIURL = "http://localhost:3000/recommend-nickname";
+
+  const form = document.getElementById("nicknameForm");
+
+  const type = form.querySelector('input[name="type"]:checked').value;
+  const searchKeyword = form.querySelector("#searchKeyword").value;
+
+  const postData = {
+    type,
+    searchKeyword,
+  };
+
+  document.getElementById("submitBtn").classList.add("loading");
+
+  fetch(APIURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application / json",
+    },
+    body: JSON.stringify(postData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const { result } = data;
+      const message = document.createElement("p");
+      message.textContent = result;
+      document.getElementById("nicknameResult").innerHTML = "";
+      document.getElementById("nicknameResult").appendChild(message);
+      document.getElementById("submitBtn").classList.remove("loading");
+    })
+    .catch((error) => {
+      console.error("오류 발생:", error);
+    });
+});
